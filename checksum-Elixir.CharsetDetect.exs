ff1486f5bd80dc918bba586bd6cbc0cf3f822bfc
Ryo Okamoto <ryo@aquahill.net>
2023-12-30 20:58:49 +0900

    added the checksum file.

---
+++ checksum-Elixir.CharsetDetect.exs
@@ -0,0 +1,12 @@
+%{
+  "charset_detect-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:7ca7575977cddb2b5ac51b8fb85425178c8406640dca28035a522830db68ed02",
+  "charset_detect-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:837edd4ba9d385e91635dd82e3b71da500740cf8e0691b666298cbbe8af1d4d4",
+  "libcharset_detect-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:21253cce89dd0f971f0e15dcb70e866d54651a3f64f4964fb1c8b3ccdacfd8c5",
+  "libcharset_detect-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ec2666caef47d1a79e8f23fcc175acaaa3dc84cc3077b2986379fc2d2d167bfe",
+  "libcharset_detect-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:f7c52277add141cea621eda667f1d50901e70e09be0e257fc443802e9c8705dc",
+  "libcharset_detect-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:ccc02c877eadc76819ec17bc536210e564bfc7742fde614bc23dda9fccc282d6",
+  "libcharset_detect-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:952a4a59149f102a669724a2512ef03254d0e6af2ad672d089e982963b97bcfb",
+  "libcharset_detect-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:d80f29a850ff6100905edf33301902b1c2321cc7feb8e3345e24aa56ead36dd1",
+  "libcharset_detect-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:84d9bf288c6e70000518cad7a95e03c4f68551d60e48e2d26adb4d5f11f524fc",
+  "libcharset_detect-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:9549821733ef72d7f604e1cda0a513df372ce1dd5aacc57e97de3e4e622ad803",
+}
