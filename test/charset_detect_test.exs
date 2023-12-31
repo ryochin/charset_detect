9f2e4a1b487117d13361ad09ef2e21ea1ad5dbeb
Ryo Okamoto <ryo@aquahill.net>
2023-12-31 10:05:05 +0900

    improved tests.

--- test/charset_detect_test.exs
+++ test/charset_detect_test.exs
@@ -2,7 +2,7 @@ defmodule CharsetDetectTest do
   use ExUnit.Case
   doctest CharsetDetect
 
-  %{
+  list = %{
     big5: "Big5",
     cp932: "Shift_JIS",
     eucjp: "EUC-JP",
@@ -11,20 +11,46 @@ defmodule CharsetDetectTest do
     sjis: "Shift_JIS",
     utf8: "UTF-8"
   }
-  |> Enum.each(fn {input, result} ->
-    @input input
-    @result result
 
-    test "#{input} encoding" do
-      assert {:ok, @result} = CharsetDetect.guess(File.read!("test/assets/#{@input}.txt"))
+  describe "guess/1" do
+    list
+    |> Enum.each(fn {input, result} ->
+      @input input
+      @result result
+
+      test "#{input} encoding" do
+        assert {:ok, @result} = CharsetDetect.guess(File.read!("test/assets/#{@input}.txt"))
+      end
+    end)
+
+    test "empty string" do
+      assert {:ok, "UTF-8"} = CharsetDetect.guess("")
     end
-  end)
 
-  test "empty string" do
-    assert {:ok, "UTF-8"} = CharsetDetect.guess("")
+    test "nil" do
+      assert {:error, "invalid argument"} = CharsetDetect.guess(nil)
+    end
   end
 
-  test "nil" do
-    assert {:error, "invalid argument"} = CharsetDetect.guess(nil)
+  describe "guess!/1" do
+    list
+    |> Enum.each(fn {input, result} ->
+      @input input
+      @result result
+
+      test "#{input} encoding" do
+        assert @result = CharsetDetect.guess!(File.read!("test/assets/#{@input}.txt"))
+      end
+    end)
+
+    test "empty string" do
+      assert "UTF-8" = CharsetDetect.guess!("")
+    end
+
+    test "nil" do
+      assert_raise ArgumentError, "argument error", fn ->
+        CharsetDetect.guess!(nil)
+      end
+    end
   end
 end
